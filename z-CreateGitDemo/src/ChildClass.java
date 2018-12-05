5b62035f8fa91d1b76096dfdf2b5ed00a5c33814
Lenovo <Lenovo@DESKTOP-77NASNU>
2018-12-05 11:28:24 +0530

    Hi this is first commit

---
+++ z-CreateGitDemo/src/ChildClass.java
@@ -0,0 +1,10 @@
+
+public class ChildClass {
+	public static void main(String[] args) {
+		
+		ParentClass pc=new ParentClass();
+		pc.show();
+		pc.eat();
+	}
+
+}
---
+++ z-CreateGitDemo/src/ParentClass.java
@@ -0,0 +1,11 @@
+
+public class ParentClass {
+	public void show () {
+		System.out.println("This is parent class invoke by child class ");
+		
+	}
+	public void eat() {
+		System.out.println("I am eating now ");
+	}
+
+}
